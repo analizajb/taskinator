95c5482f128f67662b4805c86df341e9c6d747c6
analizajb <96861913+analizajb@users.noreply.github.com>
2022-04-05 20:43:04 -0700

    Refactor the Code to Organize Functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,22 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskhandler = function (event) {
+var taskFormHandler = function (event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name= 'task-name']").value;
     var taskTypeInput = document.querySelector("select[name= 'task-type']").value;
-   
+
+    // package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    //send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+}
+
+var createTaskEl = function (taskDataObj) {
     //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
@@ -13,16 +24,14 @@ var createTaskhandler = function (event) {
     // create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-
-    // add html content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" +taskTypeInput + "</span";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span";
     listItemEl.appendChild(taskInfoEl);
 
     // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
-};
+}
 
-formEl.addEventListener("submit", createTaskhandler); {
+formEl.addEventListener("submit", taskFormHandler); {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
@@ -30,3 +39,5 @@ formEl.addEventListener("submit", createTaskhandler); {
 };
 
 //console.log(buttonEl);
+
+// 4.2.7 : Refactor the Code to Organize Functionality
