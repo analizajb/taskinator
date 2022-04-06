9904576cb02d72549a1f3ef3fee5468475387460
analizajb <96861913+analizajb@users.noreply.github.com>
2022-04-05 17:36:47 -0700

    handling form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,19 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskhandler = function () {
+var createTaskhandler = function (event) {
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
-buttonEl.addEventListener("click", createTaskhandler); {
+formEl.addEventListener("submit", createTaskhandler); {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 };
-
 
 //console.log(buttonEl);
